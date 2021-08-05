bf4a5fe09a6e29ae22431ee1ab062af979aae9f5
OnewayYoun <onewayy91@gmail.com>
2021-08-05 21:34:11 +0900

    added commenting

--- 2주차/3번(윤한길).sql
+++ 2주차/3번(윤한길).sql
@@ -9,3 +9,13 @@ ORDER BY NAME;
 -- NAME LIKE BINARY '%el%' or NAME LIKE BINARY '%El%'
 -- BINARY 옵션 추가 -> case-sensitive
 -- When we use BINARY, then mysql compare data byte-by-byte. Without BINARY it compares data character-by-character.
+
+-- Case sensitive example
+--SELECT *
+--FROM TABLE
+--WHERE Name collate SQL_Latin1_General_CP1_CS_AS like '%hospitalist%'
+
+-- Case insensitive example
+--SELECT *
+--FROM TABLE
+--WHERE Name collate SQL_Latin1_General_CP1_CI_AS like '%hospitalist%'
