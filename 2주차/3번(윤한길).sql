2f4bee964b15d1bbcd67ed93dcb48b25d062412f
OnewayYoun <onewayy91@gmail.com>
2021-08-05 21:04:47 +0900

    Create 3번(윤한길).sql

---
+++ 2주차/3번(윤한길).sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Dog' and (NAME LIKE BINARY '%el%' or NAME LIKE BINARY '%El%')
+ORDER BY NAME;
+
+-- BINARY 옵션 추가 -> case-sensitive
+-- When we use BINARY, then mysql compare data byte-by-byte. Without BINARY it compares data character-by-character.
