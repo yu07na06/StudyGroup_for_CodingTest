4963753c71891a33754d835f7d93c3cb59d0c643
OnewayYoun <onewayy91@gmail.com>
2021-08-08 02:43:57 +0900

    added another solution

--- 2주차/5번(윤한길).py
+++ 2주차/5번(윤한길).py
@@ -9,21 +9,35 @@ result
 [3, 2, 4, 1, 0]
 """
 
+# import re
+# from bisect import bisect_left, bisect_right
+#
+#
+# def solution(words, queries):
+#     new_queries = []
+#     for i in queries:
+#         new_queries.append('(?<!\w)' + i.replace('?', '\w') + '(?!\w)')
+#     # print(new_queries)
+#
+#     words.sort(key=lambda x: len(x))
+#     answer = []
+#     for i in range(len(queries)):
+#         L = bisect_left(list(map(len, words)), len(queries[i]))
+#         R = bisect_right(list(map(len, words)), len(queries[i]))
+#         answer.append(len(re.findall(new_queries[i], ' '.join(words[L:R]))))
+#
+#     return answer
+
+
 import re
 
 
 def solution(words, queries):
-    answer = []
-    queries = [i.replace('?', '\w') for i in queries]
     for i, v in enumerate(queries):
-        queries[i] = '(?<!\w)' + v + '(?!\w)'
-    # print(queries)
-    for i in queries:
-        answer.append(len(re.findall(i, ' '.join(words))))
-
-    return answer
+        queries[i] = '(?<!\w)' + v.replace('?', '\w') + '(?!\w)'
+    return [len(re.findall(i, ' '.join(words))) for i in queries]
 
 
 words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
 queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
-solution(words, queries)
+print(solution(words, queries))
