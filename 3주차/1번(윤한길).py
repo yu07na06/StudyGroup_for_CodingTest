62b0c5ff3acc3bc03651eb1991a1f4159dc11c93
OnewayYoun <onewayy91@gmail.com>
2021-08-09 14:29:05 +0900

    create 1번(윤한길).py

---
+++ 3주차/1번(윤한길).py
@@ -0,0 +1,17 @@
+"""
+진열대 번호	1	2    3	  4	  5	  6       7	       8
+보석 이름	    DIA	RUBY RUBY DIA DIA EMERALD SAPPHIRE DIA
+
+gems	                                                                result
+["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]	    [3, 7]
+["AA", "AB", "AC", "AA", "AC"]	                                        [1, 3]
+["XYZ", "XYZ", "XYZ"]	                                                [1, 1]
+["ZZZ", "YYY", "NNNN", "YYY", "BBB"]	                                [1, 5]
+"""
+
+def solution(gems):
+    answer = []
+    return answer
+
+gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+print(solution(gems))
