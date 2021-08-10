c1e96a9defc5ad47760e27457494795e0f694625
OceanShape <oceanshape700@gmail.com>
2021-08-10 23:35:48 +0900

    Create 2번(허태양).cpp

---
+++ 3주차/2번(허태양).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int N, M;
+vector<int> graph[10001];
+bool vis[10001];
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N >> M;
+  for (int i = 0; i < M; ++i) {
+    int a, b;
+    cin >> a >> b;
+    graph[b].push_back(a);
+  }
+
+  priority_queue<pair<int, int> > pq;
+  for (int i = 1; i <= N; ++i) {
+    memset(vis, false, sizeof(vis));
+    
+    int cnt = 0;
+    queue<int> q;
+    q.push(i);
+    vis[i] = true;
+    while (!q.empty()) {
+      int x = q.front();
+      q.pop();
+      ++cnt;
+      for (int j = 0; j < graph[x].size(); ++j) {
+        int a = graph[x][j];
+        if (!vis[a]) {
+          q.push(a);
+          vis[a] = true;
+        }
+      }
+    }
+    pq.push({cnt, i});
+  }
+
+  stack<int> s;
+  int max = pq.top().first;
+  while (!pq.empty()) {
+    if (pq.top().first < max) break;
+    s.push(pq.top().second);
+    pq.pop();
+  }
+
+  while (!s.empty()) {
+    cout << s.top() << ' ';
+    s.pop();
+  }
+}
