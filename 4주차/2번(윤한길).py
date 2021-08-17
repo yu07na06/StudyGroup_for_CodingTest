1257a0b75da3eacceb889dd861ebe77ad082911f
OnewayYoun <onewayy91@gmail.com>
2021-08-17 20:13:02 +0900

    create 2번(윤한길).py

---
+++ 4주차/2번(윤한길).py
@@ -0,0 +1,16 @@
+def solution(n, weak, dist):
+    answer = 0
+    return answer
+
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]
+result = 2
+solution(n, weak, dist)
+
+n = 12
+weak = [1, 3, 4, 9, 10]
+dist = [3, 5, 7]
+result = 1
+solution(n, weak, dist)
