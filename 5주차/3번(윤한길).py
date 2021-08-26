476a99c5c1e31e461a1f034551e99feed63dd255
OnewayYoun <onewayy91@gmail.com>
2021-08-26 17:22:27 +0900

    런타임에러

--- 5주차/3번(윤한길).py
+++ 5주차/3번(윤한길).py
@@ -3,3 +3,28 @@
 5 6 4 3 6 2 3
 3 2 5
 """
+
+import sys
+
+input = sys.stdin.readline
+inf = sys.maxsize
+
+D, N = map(int, input().split())
+oven = list(map(int, input().split()))
+pizza = list(map(int, input().split()))
+
+for i in range(1, D):
+    oven[i] = min(oven[i], oven[i - 1])
+
+# print(oven)
+
+idx = 0
+for i in range(D-1, -1, -1):
+    if oven[i] >= pizza[idx]:
+        if idx == len(pizza)-1:
+            answer = i+1
+            break
+        idx += 1
+
+
+print(answer)
