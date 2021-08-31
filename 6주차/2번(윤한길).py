c04ef295be3385f7a83c325a29a1ea1ffdbd0314
OnewayYoun <onewayy91@gmail.com>
2021-09-01 04:37:17 +0900

    !!! 성공 !!!

--- 6주차/2번(윤한길).py
+++ 6주차/2번(윤한길).py
@@ -6,11 +6,10 @@
 """
 import sys
 from pprint import pprint
-from bisect import bisect_left
+from copy import deepcopy
 
 
-# 입력된 번호의 물고기 위치 i, j(행, 열)로 반환해주는 함수
-def find_idx(fish_num):
+def find_idx(fish_num):     # 입력된 번호의 물고기 위치 i, j(행, 열)로 반환해주는 함수
     for i in range(4):
         try:
             j = list(map(lambda x: x[0], graph[i])).index(fish_num)
@@ -23,9 +22,10 @@ def find_idx(fish_num):
     return i, j
 
 
-# 1~16번까지의 물고기 이동하기(함수)
 def fish_move():
+
     for i in range(1, 17):
+        # pprint(graph)
         if find_idx(i):
             y, x = find_idx(i)
         elif not find_idx(i):
@@ -36,23 +36,53 @@ def fish_move():
         # 이동가능한지 체크(범위안에 있고, 물고기가 있거나 비어있는경우)
         if (0 <= new_y < 4 and 0 <= new_x < 4) and (graph[new_y][new_x][0] or not graph[new_y][new_x][1]):
             graph[y][x], graph[new_y][new_x] = graph[new_y][new_x], graph[y][x]
-            pprint(graph)
         else:
             for _ in range(7):
                 direction = (direction + 1) % 8
                 new_y, new_x = y + dy[direction], x + dx[direction]
 
                 if (0 <= new_y < 4 and 0 <= new_x < 4) and (graph[new_y][new_x][0] or not graph[new_y][new_x][1]):
+                    graph[y][x][1] = direction + 1
                     graph[y][x], graph[new_y][new_x] = graph[new_y][new_x], graph[y][x]
-                    pprint(graph)
                     break
 
+    return
+
+
+def shark_move(count):
+    global graph
+    global cnt
+    # print('상어 움직인 후:')
+    # pprint(graph)
+    fish_move()
+    # print('물고기 움직인 후:')
+    # pprint(graph)
+    cnt = max(count, cnt)
+    # print(cnt)
+    for i in range(1, 4):
+        y, x = find_idx(False)
+        direction = graph[y][x][1] - 1
+        new_y = y + (dy[direction]) * i
+        new_x = x + (dx[direction]) * i
+
+        # if (0 <= new_y < 4 and 0 <= new_x < 4) and graph[new_y][new_x][0] < 17:
+        if (0 <= new_y < 4 and 0 <= new_x < 4) and graph[new_y][new_x][0]:
+            tmp_cnt = graph[new_y][new_x][0]
+
+            tmp_g = deepcopy(graph)
+            # graph[y][x][0], graph[y][x][1], graph[new_y][new_x][0] = 17, False, False
+            graph[y][x][0], graph[y][x][1], graph[new_y][new_x][0] = None, False, False
+            shark_move(count+tmp_cnt)
+            graph = tmp_g
+            # print('백트레킹 후:')
+            # pprint(graph)
     return
 
 
 if __name__ == '__main__':
     input = sys.stdin.readline
     # [↑, ↖, ←, ↙, ↓, ↘, →, ↗]
+    # [1, 2, 3, 4, 5, 6, 7, 8]
     dx = [0, -1, -1, -1, 0, 1, 1, 1]
     dy = [-1, -1, 0, 1, 1, 1, 0, -1]
     graph = [[] for _ in range(4)]
@@ -65,11 +95,11 @@ if __name__ == '__main__':
             else:
                 graph[i][j//2].append(tmp[j])
 
+    # pprint(graph)
     # (0,0) 위치에 상어 들어가기
     cnt = 0     # 상어가 먹은 물고기 번호 총합
     cnt += graph[0][0][0]
     graph[0][0][0] = False
 
-    pprint(graph)
-    fish_move()
-    pprint(graph)
+    shark_move(cnt)
+    print(cnt)
