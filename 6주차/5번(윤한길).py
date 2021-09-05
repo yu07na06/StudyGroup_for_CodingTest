aaf4c70543eca2bbaf780897fa844ad7ea17e78a
OnewayYoun <onewayy91@gmail.com>
2021-09-05 21:10:23 +0900

    비트방식 포기하고 다른방식으로 다시 풀어야할듯..ㅜㅜ

--- 6주차/5번(윤한길).py
+++ 6주차/5번(윤한길).py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def rotate_90(input_list, clockwise_or_counterclockwise):
     if clockwise_or_counterclockwise == 'clockwise':
         rotated = list(map(list, zip(*input_list[::-1])))
@@ -27,26 +30,56 @@ def small_key(key, lock):
     return key
 
 
+def move_right(l):
+    for i in range(len(l)):
+        l[i].insert(0, 0)
+        l[i] = l[i][0:len(l)]
+
+
+def move_down(l):
+    length = len(l)
+    l.insert(0, [0] * length)
+    return l[0:length]
+
+
 def solution(key, lock):
     answer = False
     check = 2 ** (len(lock) ** 2) - 1  # ex) 1, 11, 111, 1111  xor 처리했을떄 확인하기 위한 변수
-
     rotate_tmp = small_key(key, lock)
-    print(key)
+    # print(key)
 
-    for i in range(4):
+    for _ in range(4):
         if answer:
             break
 
         rotate_tmp = rotate_90(rotate_tmp, 'clockwise')
-        tmp = list_to_binary(rotate_tmp)
-        for j in range(len(bin(tmp)) - 2):
-            if list_to_binary(lock) ^ (tmp >> j) == check:
-                answer = True
-                break
-            if list_to_binary(lock) ^ (tmp << j) == check:
-                answer = True
-                break
+
+        for i in range(len(lock)):
+            copied_tmp = deepcopy(rotate_tmp)
+            if i != 0:
+                move_right(copied_tmp)
+            for j in range(len(lock)):
+                if j != 0:
+                    copied_tmp = move_down(copied_tmp)
+                tmp = list_to_binary(copied_tmp)
+                if list_to_binary(lock) ^ tmp == check:
+                    answer = True
+                    break
+
+
+    # for i in range(4):
+    #     if answer:
+    #         break
+    #
+    #     rotate_tmp = rotate_90(rotate_tmp, 'clockwise')
+    #     tmp = list_to_binary(rotate_tmp)
+    #     for j in range(len(bin(tmp)) - 2):
+    #         if list_to_binary(lock) ^ (tmp >> j) == check:
+    #             answer = True
+    #             break
+    #         if list_to_binary(lock) ^ (tmp << j) == check:
+    #             answer = True
+    #             break
 
     return answer
 
