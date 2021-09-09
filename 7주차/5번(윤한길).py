5e03aa7d80cd11f2fb8f4282360f3c32d335e3ee
OnewayYoun <onewayy91@gmail.com>
2021-09-09 20:23:32 +0900

    regex

--- 7주차/5번(윤한길).py
+++ 7주차/5번(윤한길).py
@@ -1,7 +1,29 @@
+# def solution(s):
+#     answer = s.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', "4").replace('five', '5').replace('six', '6')\
+#         .replace('seven', '7').replace('eight', '8').replace('nine', "9").replace('zero', '0')
+#     return answer
+
+import re
+
+
+replacements = [
+    ('one', '1'),
+    ('two', '2'),
+    ('three', '3'),
+    ('four', '4'),
+    ('five', '5'),
+    ('six', '6'),
+    ('seven', '7'),
+    ('eight', '8'),
+    ('nine', '9'),
+    ('zero', '0'),
+]
+
+
 def solution(s):
-    answer = s.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', "4").replace('five', '5').replace('six', '6')\
-        .replace('seven', '7').replace('eight', '8').replace('nine', "9").replace('zero', '0')
-    return answer
+    for before, after in replacements:
+        s = re.sub(before, after, s)
+    return int(s)
 
 
 if __name__ == '__main__':
