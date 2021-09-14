67f2bd698862c66fa83123b816b6d38f56b6e317
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-09-15 01:12:12 +0900

    Update 3번(후니).cpp

--- 8주차/3번(후니).cpp
+++ 8주차/3번(후니).cpp
@@ -9,33 +9,30 @@ using namespace std;
 
 string solution(vector<string> table, vector<string> languages, vector<int> preference) {
     vector<string> tabl;
-    string answer = "";
-    string mu[25] = {};
     unordered_map<string, int> um;
     vector<unordered_map<int, int>> v;
-    int score = 1;
     
+    string answer = "";
+    
+    string a;
+    int score = 1;
     for(auto t : table) {
         unordered_map<int, int> m;
         stringstream ss(t);
-        string a;
         ss>>a;
         tabl.push_back(a);
         
         for(int i=5; i>=1; --i){
             ss>>a;
-            if(!um[a]) {
-                mu[score] = a;
-                um[a] = score++;
-            }
+            if(!um[a]) um[a] = score++;
             m[um[a]]=i;
-        } 
+        }
         
         v.push_back(m);
     }
     
     int max_score = -1;
-    int a = 0;
+    int idx = 0;
     for(auto m : v) {
         score = 0;
         for(int i=0; i<languages.size(); ++i) {
@@ -44,14 +41,13 @@ string solution(vector<string> table, vector<string> languages, vector<int> pref
         
         if(max_score < score) {
             max_score = score;
-            answer = tabl[a];
-            // cout << score << " :: " << m.begin()->first << ", " << m.begin()->second << " :: " << mu[m.begin()->first] << ", " << mu[m.begin()->second] << '\n';
+            answer = tabl[idx];
+            
         } else if(max_score == score) {
-            if(answer > tabl[a]) {
-                answer = tabl[a];
-            }
+            answer = min(answer, tabl[idx]);
         }
-        a++;
+        
+        idx++;
     }
         
     return answer;
