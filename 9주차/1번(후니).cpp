8de331f9af32b5c1b15c5926aa9c68aa6b84b73a
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-09-20 12:16:27 +0900

    Update 1번(후니).cpp

--- 9주차/1번(후니).cpp
+++ 9주차/1번(후니).cpp
@@ -1,1 +1,25 @@
-
+// 20210920 1200-1215
+#include <iostream>
+#include <string>
+using namespace std;
+int ans, done;
+string alphabet[5]={"A","E","I","O","U"};
+void dfs(string str, string word) {
+    // cout<< ans << " : " << str<< '\n';
+    if(str == word) {
+        done++;
+        return;
+    }
+    if(str.size() == 5) return;
+    
+    for(int i=0; i<5; ++i) {
+        if(done) return;
+        ans++;
+        dfs(str+alphabet[i], word);
+    }
+    
+}
+int solution(string word) {
+    dfs("", word);
+    return ans;
+}
