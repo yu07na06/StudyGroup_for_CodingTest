a05eca53ad3b822fa054ab3c3639f7724ddb5009
OnewayYoun <onewayy91@gmail.com>
2021-09-27 23:34:39 +0900

    create files

---
+++ 10주차/1번(윤한길).py
@@ -0,0 +1,14 @@
+def solution(sizes):
+    answer = 0
+    return answer
+
+
+if __name__ == '__main__':
+    sizes, result = [[60, 50], [30, 70], [60, 30], [80, 40]], 4000
+    print(solution(sizes))
+
+    sizes, result = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]], 120
+    print(solution(sizes))
+
+    sizes, result = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]], 133
+    print(solution(sizes))
---
+++ 10주차/2번(윤한길).py
@@ -0,0 +1,14 @@
+def solution(lottos, win_nums):
+    answer = []
+    return answer
+
+
+if __name__ == '__main__':
+    lottos, win_nums, result = [44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19], [3, 5]
+    print(solution(lottos, win_nums))
+
+    lottos, win_nums, result = [0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25], [1, 6]
+    print(solution(lottos, win_nums))
+
+    lottos, win_nums, result = [45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35], [1, 1]
+    print(solution(lottos, win_nums))
